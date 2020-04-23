98613b40678ef4436481076a24df1bb5426e7845
huanghanzhi <274962828@qq.com>
2020-04-23 19:03:12 +0800

    [Change] 优化代码

--- UnityProj/Assets/DialogTest/DialogPanel.cs
+++ UnityProj/Assets/DialogTest/DialogPanel.cs
@@ -6,75 +6,38 @@ using UnityEngine.UI;
 
 public class DialogPanel : UIBase
 {
-    private TMP_Text _sentenceTxt;
     private int _currentIndex = 0;
-    private Coroutine _showSentenceCoroutine;
 
-    private Sentence _currentSentence;
+    private SentenceItem _sentenceItem;
 
     void Awake()
     {
         DialogCore.Instance.Init();
-        _sentenceTxt = transform.Find<TMP_Text>("ContentPanel/SentenceTxt");
+        _sentenceItem = transform.Find<SentenceItem>("ContentPanel/SentenceTxt");
     }
 
     void Start()
     {
-        ShowSentence();
+        DisplaySentence();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ShowSentence();
+            DisplaySentence();
         }
     }
 
-    private void ShowSentence()
+    private void DisplaySentence()
     {
-        if (_showSentenceCoroutine != null)
+        if (!_sentenceItem.IsFinished())
         {
-            ResetCoroutine();
-            _sentenceTxt.maxVisibleCharacters = _sentenceTxt.textInfo.characterCount;
-            if (_currentSentence.TriggerCount != -1)
-            {
-                _currentSentence.TriggerCallback?.Invoke();
-            }
+            _sentenceItem.DisplayImmediately();
             return;
         }
 
-        _currentSentence = DialogCore.Instance.GetSentenceByIndex(_currentIndex++ % DialogCore.Instance.GetSentenceCount());
-        _sentenceTxt.text = _currentSentence.Content;
-        _showSentenceCoroutine = StartCoroutine(ShowSentenceCoroutine());
-    }
-
-    private IEnumerator ShowSentenceCoroutine()
-    {
-        _sentenceTxt.ForceMeshUpdate();
-        TMP_TextInfo sentenceInfo = _sentenceTxt.textInfo;
-        int totalCharacters = sentenceInfo.characterCount;
-        int currentVisibleCount = 0;
-        while (currentVisibleCount <= totalCharacters)
-        {
-            _sentenceTxt.maxVisibleCharacters = currentVisibleCount;
-            if (currentVisibleCount == _currentSentence.TriggerCount)
-            {
-                _currentSentence.TriggerCallback?.Invoke();
-            }
-            currentVisibleCount++;
-            yield return new WaitForSeconds(_currentSentence.NormalInterval);
-        }
-
-        if (_showSentenceCoroutine != null)
-        {
-            ResetCoroutine();
-        }
-    }
-
-    private void ResetCoroutine()
-    {
-        StopCoroutine(_showSentenceCoroutine);
-        _showSentenceCoroutine = null;
+        _sentenceItem.Sentence = DialogCore.Instance.GetSentenceByIndex(_currentIndex++ % DialogCore.Instance.GetSentenceCount());
+        _sentenceItem.DisplayNormally();
     }
 }
---
+++ UnityProj/Assets/DialogTest/SentenceItem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SentenceItem : MonoBehaviour
+{
+    private TMP_Text _sentenceTxt;
+    private Coroutine _displayCoroutine;
+
+    public Sentence Sentence { set; get; }
+
+    void Awake()
+    {
+        _sentenceTxt = GetComponent<TMP_Text>();
+    }
+
+    public bool IsFinished()
+    {
+        return _displayCoroutine == null;
+    }
+
+    public void DisplayNormally()
+    {
+        _sentenceTxt.text = Sentence.Content;
+        _displayCoroutine = StartCoroutine(DisplayCoroutine());
+    }
+
+    private IEnumerator DisplayCoroutine()
+    {
+        _sentenceTxt.ForceMeshUpdate();
+        TMP_TextInfo sentenceInfo = _sentenceTxt.textInfo;
+        int totalCharacters = sentenceInfo.characterCount;
+        int currentVisibleCount = 0;
+        while (currentVisibleCount <= totalCharacters)
+        {
+            _sentenceTxt.maxVisibleCharacters = currentVisibleCount;
+            if (currentVisibleCount == Sentence.TriggerCount)
+            {
+                Sentence.TriggerCallback?.Invoke();
+            }
+            currentVisibleCount++;
+            yield return new WaitForSeconds(Sentence.NormalInterval);
+        }
+
+        if (_displayCoroutine != null)
+        {
+            ResetCoroutine();
+        }
+    }
+
+    public void DisplayImmediately()
+    {
+        ResetCoroutine();
+        _sentenceTxt.maxVisibleCharacters = _sentenceTxt.textInfo.characterCount;
+        if (Sentence.TriggerCount != -1)
+        {
+            Sentence.TriggerCallback?.Invoke();
+        }
+    }
+
+    private void ResetCoroutine()
+    {
+        StopCoroutine(_displayCoroutine);
+        _displayCoroutine = null;
+    }
+}
